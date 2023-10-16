ba6a85dc1394bf325755cb1316de863efe644fde
Rui Dias <rui.dias@outsystems.com>
2023-10-16 19:21:04 +0100

    foo

--- card.js
+++ card.js
@@ -51,88 +51,89 @@ class TransportMapCard extends HTMLElement {
         if (!config.entity) {
           throw new Error("You need to define an entity");
         }
+        this.config = config;
 
-        const root = this.shadowRoot;
-        if (root.lastChild) root.removeChild(root.lastChild);
+        // const root = this.shadowRoot;
+        // if (root.lastChild) root.removeChild(root.lastChild);
     
-        const cardConfig = Object.assign({}, config);
-        const card = document.createElement('ha-card');
-        const content = document.createElement('div');
-        const style = document.createElement('style');
+        // const cardConfig = Object.assign({}, config);
+        // const card = document.createElement('ha-card');
+        // const content = document.createElement('div');
+        // const style = document.createElement('style');
 
-        style.textContent = `
-            ha-card {
-                /* sample css */
-            }
-            .timeline-container {
-                display: flex;
-                flex-direction: column;
-                position: relative;
-                margin: 40px 0;
-            }
-            .timeline-container::after {
-                background-color: #e17b77;
-                content: '';
-                position: absolute;
-                left: calc(50% - 2px);
-                width: 4px;
-                height: 100%;
-            }
-            .timeline-item {
-                display: flex;
-                justify-content: flex-end;
-                padding-right: 30px;
-                position: relative;
-                margin: 10px 0;
-                width: 50%;
-            }
-            .timeline-item:nth-child(odd) {
-                align-self: flex-end;
-                justify-content: flex-start;
-                padding-left: 30px;
-                padding-right: 0;
-            }
-            .timeline-item-content {
-                box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
-                border-radius: 5px;
-                background-color: #fff;
-                display: flex;
-                flex-direction: column;
-                align-items: flex-end;
-                padding: 15px;
-                position: relative;
-                width: 400px;
-                max-width: 70%;
-                text-align: right;
-            }
-            .timeline-item-content::after {
-                content: ' ';
-                background-color: #fff;
-                box-shadow: 1px -1px 1px rgba(0, 0, 0, 0.2);
-                position: absolute;
-                right: -7.5px;
-                top: calc(50% - 7.5px);
-                transform: rotate(45deg);
-                width: 15px;
-                height: 15px;
-            }
-            .timeline-item:nth-child(odd) .timeline-item-content {
-                text-align: left;
-                align-items: flex-start;
-            }
-            .timeline-item:nth-child(odd) .timeline-item-content::after {
-                right: auto;
-                left: -7.5px;
-                box-shadow: -1px 1px 1px rgba(0, 0, 0, 0.2);
-            }
-        `;
+        // style.textContent = `
+        //     ha-card {
+        //         /* sample css */
+        //     }
+        //     .timeline-container {
+        //         display: flex;
+        //         flex-direction: column;
+        //         position: relative;
+        //         margin: 40px 0;
+        //     }
+        //     .timeline-container::after {
+        //         background-color: #e17b77;
+        //         content: '';
+        //         position: absolute;
+        //         left: calc(50% - 2px);
+        //         width: 4px;
+        //         height: 100%;
+        //     }
+        //     .timeline-item {
+        //         display: flex;
+        //         justify-content: flex-end;
+        //         padding-right: 30px;
+        //         position: relative;
+        //         margin: 10px 0;
+        //         width: 50%;
+        //     }
+        //     .timeline-item:nth-child(odd) {
+        //         align-self: flex-end;
+        //         justify-content: flex-start;
+        //         padding-left: 30px;
+        //         padding-right: 0;
+        //     }
+        //     .timeline-item-content {
+        //         box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
+        //         border-radius: 5px;
+        //         background-color: #fff;
+        //         display: flex;
+        //         flex-direction: column;
+        //         align-items: flex-end;
+        //         padding: 15px;
+        //         position: relative;
+        //         width: 400px;
+        //         max-width: 70%;
+        //         text-align: right;
+        //     }
+        //     .timeline-item-content::after {
+        //         content: ' ';
+        //         background-color: #fff;
+        //         box-shadow: 1px -1px 1px rgba(0, 0, 0, 0.2);
+        //         position: absolute;
+        //         right: -7.5px;
+        //         top: calc(50% - 7.5px);
+        //         transform: rotate(45deg);
+        //         width: 15px;
+        //         height: 15px;
+        //     }
+        //     .timeline-item:nth-child(odd) .timeline-item-content {
+        //         text-align: left;
+        //         align-items: flex-start;
+        //     }
+        //     .timeline-item:nth-child(odd) .timeline-item-content::after {
+        //         right: auto;
+        //         left: -7.5px;
+        //         box-shadow: -1px 1px 1px rgba(0, 0, 0, 0.2);
+        //     }
+        // `;
 
-        content.id = "container";
-        cardConfig.title ? card.header = cardConfig.title : null;
-        card.appendChild(content);
-        card.appendChild(style);
-        root.appendChild(card);
-        this.config = cardConfig;        
+        // content.id = "container";
+        // cardConfig.title ? card.header = cardConfig.title : null;
+        // card.appendChild(content);
+        // card.appendChild(style);
+        // root.appendChild(card);
+        // this.config = cardConfig;
     }
 
     // TimelineItem = ({ data }) => (
